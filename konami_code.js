a63951967d9634f7a485074dfd262a6413521847
Marissa Zhong <mwz2101@columbia.edu>
2018-06-24 15:16:52 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,18 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index = 0;
+  function onKeyDownHandler(e) {
+    const key = e.key;
+    if (key === codes[index]) {
+      index += 1;
+      if (index === codes.length) {
+        alert("Congratulations!");
+        index = 0
+      }
+    } else {
+      index = 0;
+    }
+  }
+  return document.body.addEventListener('keydown', onKeyDownHandler(e))
 }
